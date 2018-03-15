8a3277384569ce330d2cbf6bb3843f2b2d1f597e
Stanislav Lechev [AngelFire] <af@0xAF.org>
2018-03-15 11:58:32 +0200

    alpine...

--- init.sh
+++ init.sh
@@ -25,7 +25,7 @@ if [ -f /etc/alpine-release ]; then
 		echo "run me with root."
 		exit 1
 	fi
-	packages="bash vim git make"
+	packages="bash vim git make findutils wget coreutils"
 	apk add -t af-etc $packages
 fi
 
