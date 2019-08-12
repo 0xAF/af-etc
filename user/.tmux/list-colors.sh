4dba20d4afc73ed01308f02df5221f30eeba4358
Stanislav Lechev [AngelFire] <af@0xAF.org>
2019-08-13 01:04:18 +0300

    updates

--- user/.tmux/list-colors.sh
+++ user/.tmux/list-colors.sh
@@ -1,4 +1,12 @@
-for i in {0..255}; do
-	printf "\x1b[38;5;${i}mcolour${i}\x1b[0m\n"
-done
+#!/bin/bash
 
+rows=32
+cols=$(( 256 / $rows))
+
+for ((i1=0; i1 < $rows; i1++)); do
+	for ((i2=0; i2 < $cols; i2++)); do
+		a=$(($i2 * $rows + $i1))
+		printf "\x1b[38;5;${a}m%-10s" "color${a}";
+	done
+	echo
+done
